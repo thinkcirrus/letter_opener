628dc3d4db1a68e3bd678970fe6320fdb5f2e153
andy <mailto.alekseenko@gmail.com>
2016-10-25 12:49:05 +0000

    Fixed routes for rails 4; fixed gitignore; removed rvmrc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  match "/letter_opener"                => 'letter_opener/letters#index', :as => :letter_opener_letters
-  match "/letter_opener/:id/:style.html" => 'letter_opener/letters#show', :as => :letter_opener_letter
+  get '/letter_opener', to: 'letter_opener/letters#index', as: :letter_opener_letters
+  get '/letter_opener/:id/:style.html', to: 'letter_opener/letters#show', as: :letter_opener_letter
 end
